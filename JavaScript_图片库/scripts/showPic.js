ce0f6270068c5902a173387f4651e7544dee35bc
archy <hq_dev@163.com>
2016-07-19 23:27:18 +0800

    图片库改进

--- JavaScript_图片库/scripts/showPic.js
+++ JavaScript_图片库/scripts/showPic.js
@@ -11,5 +11,33 @@ function countBodyChildren() {
 	var body_element = document.getElementsByTagName("body")[0];
 	alert(body_element.childNodes.length);
 }
-  window.onload = countBodyChildren;
+
+function prepareGallery() {
+	// 具有普遍适用性的测试
+	if (!document.getElementById || !document.getElementsByTagName) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	for (vat i = 0; i < links.length; i++) {
+		links[i].onclick = function() {
+			showPic(this);
+			return false; // 意思：按照这个链接没被点击的情况采取行动
+		}
+	}
+}
+
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.inload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(countBodyChildren);
+addLoadEvent(addLoadEvent);
 
